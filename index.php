1cc749f76663709d1bb0fedfd6a4fabb4f0fb0eb
webdevxru <webdevx@mail.ru>
2023-09-01 20:59:46 +0300

    added theme folder

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
 ?>
-
+content
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
