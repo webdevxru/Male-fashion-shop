94cd92b0f83d894b574d44eb9b372621d950603c
webdevxru <webdevx@mail.ru>
2023-09-03 09:55:08 +0300

    added component menu + menu_ext

--- local/templates/men-shop-index/header.php
+++ local/templates/men-shop-index/header.php
@@ -117,7 +117,24 @@
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6">
-                    <nav class="header__menu mobile-menu">
+              
+		 <?$APPLICATION->IncludeComponent(
+	"bitrix:menu",
+	"",
+	Array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "dopmenu",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "2",
+		"MENU_CACHE_GET_VARS" => array(""),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "N",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"ROOT_MENU_TYPE" => "main",
+		"USE_EXT" => "Y"
+	)
+);?>
+                    <!-- <nav class="header__menu mobile-menu">
                         <ul>
                             <li class="active"><a href="./index.html">Home</a></li>
                             <li><a href="./shop.html">Shop</a></li>
@@ -133,7 +150,7 @@
                             <li><a href="./blog.html">Blog</a></li>
                             <li><a href="./contact.html">Contacts</a></li>
                         </ul>
-                    </nav>
+                    </nav> -->
                 </div>
                 <div class="col-lg-3 col-md-3">
                     <div class="header__nav__option">
